5b43d6b860b4d01035ea71dad434d3107eb175ac
Steven.Bruce <sbruce@tql.com>
2021-10-07 17:03:49 -0400

    end of asset management ready for Output Management Guide

--- src/index.js
+++ src/index.js
@@ -1,24 +1,10 @@
 import _ from 'lodash';
-import './style.css';
-import Tiger from './tiger_smoke.jpg';
-import Data from './data.xml';
-import Notes from './data.csv';
 
 function component() {
     const element = document.createElement('div');
 
     // Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
-
-    // Add the image to our existing div.
-    const myImage = new Image();
-    myImage.src = Tiger;
-
-    element.appendChild(myImage);
-
-    console.log(Data);
-    console.log(Notes);
 
     return element;
 }
