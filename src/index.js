eb538aba767be44172b651665c3d88d32d7a74a8
Steven Bruce <brucesc726@gmail.com>
2021-10-08 12:21:37 -0400

    code splitting dynamic import

--- src/index.js
+++ src/index.js
@@ -1,19 +1,16 @@
-import _ from 'lodash';
-import printMe from './print.js';
+function getComponent() {
+    return import('lodash')
+        .then(({ default: _ }) => {
+            const element = document.createElement('div');
 
-function component() {
-    const element = document.createElement('div');
-    const btn = document.createElement('button');
-
-    // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-    btn.innerHTML = 'Click me and check the console!';
-    btn.onclick = printMe;
-
-    element.appendChild(btn);
+            element.innerHTML = _.join(['Hello', 'Steven'], ' ');
+            return element;
+        })
+        .catch((error) => 'An error occurred while loading the component');
     
     return element;
 }
 
-document.body.appendChild(component());
+getComponent().then((component) => {
+    document.body.appendChild(component);
+});
