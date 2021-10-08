77f7fef6ada135a35aee9759711de79e705d92a2
Steven Bruce <brucesc726@gmail.com>
2021-10-08 12:36:54 -0400

    dynamic import with async/await

--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
-function getComponent() {
-    return import('lodash')
-        .then(({ default: _ }) => {
-            const element = document.createElement('div');
+async function getComponent() {
+    const element = document.createElement('div');
+    const { default: _ } = await import('lodash');
 
-            element.innerHTML = _.join(['Hello', 'Steven'], ' ');
-            return element;
-        })
-        .catch((error) => 'An error occurred while loading the component');
-    
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
     return element;
 }
 
